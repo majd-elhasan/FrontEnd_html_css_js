91327b7a13acc375e041b1bb010bee5c0cc1ebce
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-04-24 03:09:54 +0300

    first_commit
    invoke function by btn id

--- javaScript/asiankitchen/app.js
+++ javaScript/asiankitchen/app.js
@@ -81,3 +81,68 @@ const menu = [
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
+var IMC = document.querySelector("#IMC");
+  let main_div = document.createElement("div");
+  main_div.classList = ["col-lg-6 col-sm-12 menu-items"];
+  var img = new Image();
+  img.className ="photo"
+  var menu_info = document.createElement("div");
+  menu_info.className =" menu-info"
+  var menu_title = document.createElement("div");
+  menu_title.className = "menu-title"
+    var title_value = document.createElement("h4");
+    var price = document.createElement("h4");
+    price.className="price"
+  menu_text = document.createElement("div")
+// <!-- single-item -->
+//           <div class="col-lg-6 col-sm-12 menu-items">
+//             <img src="" alt="">
+//             <div class="menu-info">
+//               <div class="menu-title">
+//                 <h4>the title Value</h4>
+//                 <h4 class="price">00.0</h4>
+//               </div>
+//               <div class="menu-text"></div>
+//             </div>
+//           </div>
+//           <!-- end of single item -->
+var insert_HTML_elements = function(){
+  IMC.innerHTML += "<!-- single-item -->";
+  IMC.appendChild(main_div);
+    main_div.appendChild(img);
+    main_div.appendChild(menu_info);
+      menu_info.appendChild(menu_title);
+        menu_title.appendChild(title_value);
+        menu_title.appendChild(price);
+      menu_info.appendChild(menu_text);
+  IMC.innerHTML += "<!-- end of single item -->";
+
+}
+
+
+var btn_func = function(category = "all"){
+  IMC.innerHTML = ""
+
+  menu.forEach(item => {
+    if(item.category == category || category == "all")
+    {
+      img.src = item.img;
+      title_value.innerHTML = item.title;
+      price .innerHTML = item.price;
+      menu_text.innerHTML = item.desc;
+
+      insert_HTML_elements();
+    }
+  })
+};
+
+var btn_click =(id)=> btn_func(id);
+
+// document.querySelector("#all").addEventListener("click", () => btn_func());
+// document.querySelector("#Korea").addEventListener("click", () => btn_func("Korea"));
+// document.querySelector("#Japan").addEventListener("click", () => btn_func("Japan"));
+// document.querySelector("#China").addEventListener("click", () => btn_func("China"));
+
+
+
